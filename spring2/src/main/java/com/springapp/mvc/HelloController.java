c15c54ba8cef6d1e9eeb63693317d948355aa47b
Sajit Mathew Kunnumkal <sajit.kunnumkal@accenturefederal.com>
2016-01-18 11:15:52 -0500

    Heroes json response

--- spring2/src/main/java/com/springapp/mvc/HelloController.java
+++ spring2/src/main/java/com/springapp/mvc/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -12,5 +16,17 @@ public class HelloController {
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Angular 2 & Spring!");
 		return "hello";
+	}
+
+	static List<Hero> heros = new ArrayList<Hero>();
+	static {
+		heros.add(new Hero(1,"Captain America"));
+		heros.add(new Hero(2,"Ironman"));
+	}
+
+	@RequestMapping(value = "/heroes", method = RequestMethod.GET,headers="Accept=application/json")
+	@ResponseBody
+	public List<Hero> getHeroes(){
+		return heros;
 	}
 }
