2d1bd1a36ff92bbdf7e25c73e808ad8c3a13436c
sajit <sajit.kunnumkal@gmail.com>
2016-04-21 18:18:51 -0400

    wip

--- heroes/app/hero-detail.component.ts
+++ heroes/app/hero-detail.component.ts
@@ -1,22 +1,10 @@
 import {Component, Input} from 'angular2/core';
 import {Hero} from './hero';
-
 @Component({
-    selector: 'my-hero-detail',
-    template: `
-
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-  `
+  selector: 'my-hero-detail',
+  templateUrl: 'views/hero-details.html' 
 })
 export class HeroDetailComponent {
-    @Input()
-        hero: Hero;
+  @Input() 
+  hero: Hero;
 }
-
